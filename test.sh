0933eb010aa21e2f989c3065109d7e80d6aa8553
Zeng Heng <henryZ_e@163.com>
2022-09-09 22:08:24 +0800

    5: support more punctuators: *, / and ()

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CROSS_COMPILE=riscv64-linux-gnu-
 
 assert()
 {
@@ -23,5 +24,8 @@ assert 0 0
 assert 42 42
 assert 21 '5+20-4'
 assert 41 ' 12 + 34 - 5 '
+assert 47 '5+6*7'
+assert 15 '5*(9-6)'
+assert 4 '(3+5)/2'
 
 echo ok
