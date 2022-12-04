ff9a1e8e5367c563544f63a034c5f844a28befce
Zeng Heng <henryZ_e@163.com>
2022-12-05 00:25:05 +0800

    63: Add 'long long' as an alias for 'long'

--- test/decl.c
+++ test/decl.c
@@ -8,6 +8,7 @@ int main()
 	ASSERT(4, ({ int x; sizeof(x); }));
 	ASSERT(8, ({ long int x; sizeof(x); }));
 	ASSERT(8, ({ int long x; sizeof(x); }));
+	ASSERT(8, ({ long long x; sizeof(x); }));
 
 	printf("OK\n");
 	return 0;
