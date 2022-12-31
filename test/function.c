1939e94c58b8a5e1d575dca304ae38aae99525a3
Zeng Heng <henryZ_e@163.com>
2022-12-31 23:44:43 +0800

    69: Handle function return type conversion

--- test/function.c
+++ test/function.c
@@ -48,6 +48,18 @@ int fib(int x)
 	return fib(x - 1) + fib(x - 2);
 }
 
+int g1;
+
+int *g1_ptr()
+{
+	return &g1;
+}
+
+char int_to_char(int x)
+{
+	return x;
+}
+
 int main()
 {
 	ASSERT(3, ret3());
@@ -65,6 +77,10 @@ int main()
 
 	ASSERT(1, sub_long(7, 3, 3));
 	ASSERT(1, sub_short(7, 3, 3));
+
+	g1 = 3;
+	ASSERT(3, *g1_ptr());
+	ASSERT(5, int_to_char(261));
 
 	printf("OK\n");
 	return 0;
