da758fcc18952d152604cdd705ed4abd7d1396bf
Zeng Heng <henryZ_e@163.com>
2023-04-06 15:13:39 +0800

    119: Allow a variable as an operand of _Alignof

--- parser.c
+++ parser.c
@@ -394,6 +394,7 @@ static bool is_typename(struct Token *tok)
 //	| "sizeof" "(" type-name ")"
 // 	| "sizeof" unary
 //	| "_Alignof" "(" type-name ")"
+//	| "_Alignof" unary
 // 	| ident (func-args)?
 // 	| str
 // 	| num
@@ -431,12 +432,19 @@ static struct Node *primary(struct Token **rest, struct Token *tok)
 		return new_num(node->ty->size, tok);
 	}
 
-	if (equal(tok, "_Alignof")) {
-		tok = skip(tok->next, "(");
-		struct Type *ty = typename(&tok, tok);
-		*rest = skip(tok, ")");
+	if (equal(tok, "_Alignof") && equal(tok->next, "(") &&
+	    is_typename(tok->next->next)) {
 
+		struct Type *ty = typename(&tok, tok->next->next);
+		*rest = skip(tok, ")");
 		return new_num(ty->align, tok);
+	}
+
+	if (equal(tok, "_Alignof")) {
+		struct Node *node = unary(rest, tok->next);
+		add_type(node);
+
+		return new_num(node->ty->align, tok);
 	}
 
 	if (tok->kind == TK_IDENT) {
