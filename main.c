f3a7c58ee38d1ff19715cc613bfca3df5fca7093
Zeng.Heng <henryZ_e@163.com>
2024-04-20 00:13:51 +0800

    2: add + & - operators

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main(int argc, char **argv)
 {
+	char *p = argv[1];
+
 	if (argc != 2) {
 		fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
 		return 1;
@@ -11,8 +13,22 @@ int main(int argc, char **argv)
 	printf(".global main\n");
 	printf("main:\n");
 
-	printf("\tli a0, %d\n", atoi(argv[1]));
-	printf("\tret\n");
+	printf("\tli a0, %ld\n", strtol(p, &p, 10));
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("\tadd a0, a0, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		if (*p == '-') {
+			p++;
+			printf("\tadd a0, a0, %ld\n", -(strtol(p, &p, 10)));
+			continue;
+		}
+		fprintf(stderr, "unexpected character: '%c'\n", *p);
+		return 1;
+	}
 
+	printf("\tret\n");
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -27,5 +27,6 @@ assert()
 
 assert 0 0
 assert 42 42
+assert 21 '5+20-4'
 
 echo ok
